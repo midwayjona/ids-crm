649165434000de2439dc1a7f2f71b68211693792
Jonathan Amado <midwayjona@gmail.com>
2020-10-28 21:00:34 -0600

    update

--- api/costumer/create.php
+++ api/costumer/create.php
@@ -4,8 +4,8 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type,
-Access-Control-Allow-Methods, Authorization, X-Requested-With, Origin');
+header('Access-Control-Allow-Headers: Origin, Access-Control-Allow-Headers, Content-Type,
+Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../config/database.php';
 include_once '../../models/costumer.php';
--- api/sale/create.php
+++ api/sale/create.php
@@ -4,7 +4,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type,
+header('Access-Control-Allow-Headers: Origin, Access-Control-Allow-Headers, Content-Type,
 Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../config/database.php';
